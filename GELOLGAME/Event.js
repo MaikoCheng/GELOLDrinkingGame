8f9e028c9ff71e48bbb43809990fcdc33ffbedd2
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-05-25 02:01:50 +0200

    Add files via upload

---
+++ GELOLGAME/Event.js
@@ -0,0 +1,260 @@
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+var array = ["Start: Welcome to the game! You are at Jort and Maiko's appartment, the hangout place of GELØL!",
+"1: This is the gas station you see when exiting the appartment, do a warmup shot!",
+"2: This is the bicycle route to TU/e, show an embarassing picture of yourself or take 5 sips!",
+"3: Welcome to TU/e, drink 3 sips to celebrate you made it here!",
+"4: Give the amount of levels (10) in Atlas away in sips!",
+"5: This is where you meet your friends! Choose someone to be your drinking buddy (drink the same the entire game)!",
+"6: The BAC hosts a Borrel, where you meet Cas! Go to moetikeenriet.at!",
+"7: You are safe, for now!",
+"8: You burned so many calories while sporting, give away 5 sips!",
+"9: Gefontyst, drink away your tears with 5 shots!",
+"10: You almost got shanked while you biked in Woensel, take a shot to get rid of the PTSD!",
+"11: In Woensel, Marlou accompanies you playing Megan Thee Stallion on her speaker. Do your best Ahh! impression or take 3 sips!",
+"12: You are hungry! Let's get food at Çigköftem. Give away 5 sips!",
+"13: Oh no! You spilled your epic gamer juice while playing fortnite! Take 5 sips!",
+"14: You are safe, for now!",
+"15: You go to the station after the protests and your bike got stolen. Everybody who has had their bike stolen takes 5 sips!",
+"16: You go to Thomas' place to get hamburgers, there, he hid an ICE for you! Take 5 sips!",
+"17: You go to the music store to grab some new equipment. You get a contract offer while playing piano! Give away 10 sips (in total) to 2 people!",
+"18: You see Poyraz sitting next to you with his guitar, you make a top 10 hit! Everybody takes 3 sips to celebrate!",
+"19: You realise you forgot your laptop at GEWIS, skip a turn!",
+"20: You visit PSV but realise that they didn't win the title so you get sad. Take 5 sips!",
+"21: You are safe, for now!",
+"22: You go to Oriental to buy some Soju! Take 5 sips if you had asian quisine in the last week!",
+"23: You see Maiko, Mathew and Willem stand in Oriental ordering boba, everybody who had boba in the last 2 weeks takes 6 sips!",
+"24: If you see Willem, Ilse can't be far! There she is, standing there with a backpack full of ICEs! Take a shot if you haven't been iced in 2 weeks!",
+"25: You have a flat tire. Skip a turn and take 5 sips!",
+"26: You were just on time for the bus! Play a quick 4 card bus game!",
+"27: While on the bus, you see Jort so as usual, you sit on his lap! Go sit on Jort's lap or take 5 sips!",
+"28: You are safe, for now!",
+"29: You get some weed from Pink but you got scammed! Everyone who has smoked weed in the last week can give away 5 sips!",
+"30: Now that you have your weed, you have to look for something to do with it! Casper has the best brownie recipes! Give away a shot!",
+"31: You go to Stratumseind to drink, take 5 sips to pregame!",
+"32: You are super drunk and almost barfed! Everyone takes a shot if they have barfed in the last month!",
+"33: You are down bad, if you don't have a relationship or had sex in the last 2 weeks, take a shot of 43!",
+"34: Your condom ripped, choose someone to be the mommy/daddy. You both take a shot!",
+"35: You are safe, for now!",
+"36: Milo just introduced you to some new games! Give away 10 sips (in total) to 3 people!",
+"37: Loek just fed you some shots! Take the last one!",
+"38: You got caught while biking while curfew was going on! Go back to day 28!",
+"39: You have yogamat duty! Skip 2 turns to get back some sleep!",
+"40: You blacked out and the ambulance had to take you to the hospital. They crashed and left you back at Jort and Maiko's place!"
+
+]
+document.getElementById("vak0but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[0];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak1but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[1];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak2but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[2];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak3but").onclick = function(){
+    document.getElementById("modtext").innerHTML =  array[3];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak4but").onclick = function(){
+    document.getElementById("modtext").innerHTML =  array[4];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak5but").onclick = function(){
+    document.getElementById("modtext").innerHTML =  array[5];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak6but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[6];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak7but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[7];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak8but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[8];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak9but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[9];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak10but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[10];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak11but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[11];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak12but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[12];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak13but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[13];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak14but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[14];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak15but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[15];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak16but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[16];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak17but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[17];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak18but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[18];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak19but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[19];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak20but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[20];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak21but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[21];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak22but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[22];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak23but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[23];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak24but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[24];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak25but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[25];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak26but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[26];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak27but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[27];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak28but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[28];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak29but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[29];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak30but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[30];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak31but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[31];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak32but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[32];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak33but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[33];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak34but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[34];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak35but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[35];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak36but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[36];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak37but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[37];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak38but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[38];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak39but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[39];
+    modal.style.display = "block"
+}
+
+document.getElementById("vak40but").onclick = function(){
+    document.getElementById("modtext").innerHTML = array[40];
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
